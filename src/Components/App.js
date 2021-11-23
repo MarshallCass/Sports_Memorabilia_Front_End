18330962e4a8214817af04e7bae7a6f4053b22a0
Ben Fischer <fischerben010@gmail.com>
2021-11-23 16:00:16 -0600

    Changed the formatting of the file

--- src/Components/App.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import './Styles/App.css';
-import { BrowserRouter as Switch, Route, Router } from "react-router-dom";
-import { Container, Row, Col } from 'react-bootstrap';
-import axios from 'axios';
