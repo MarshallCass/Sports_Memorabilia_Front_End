35be0a56edc20ccc8e7f3c896b38726fbf5b2030
Ben Fischer <fischerben010@gmail.com>
2021-11-24 10:06:14 -0600

    Additional parts for login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -11,22 +11,30 @@ class Login extends Component {
         };
     }
 
-    handleChange
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <h3>Login</h3>
 
                     <div className="form-group">
                         <label>Username</label>
-                        <input type="text" className="form-control" placeholder="Username" />
+                        <input type="text" className="form-control" onChange={this.handleChange} value={this.state.username} />
                     </div>
                     <br />
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Password" />
+                        <input type="password" className="form-control" onChange={this.handleChange} value={this.state.password} />
                     </div>
                     <br />
                     <button type="submit" className="btn btn-primary btn-block">Submit</button>
