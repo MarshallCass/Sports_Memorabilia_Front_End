c7082332b503107a5aa7599d7eb3617531f37a65
Marshall C <MCass2101@gmail.com>
2021-11-24 13:34:40 -0500

    updated styling on registration alittle more

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -42,27 +42,61 @@ class Register extends Component {
 
     render() {
         return (
-           <div className="login-wrapper">
-               <h3>Register</h3>
-               <form onSubmit={this.handleSubmit}>
-                   <table>
-                       <label>First Name</label>
-                       <input type = "text" name= "firstname" onChange={this.handleChange} value={this.state.firstname} ></input>
-                       <label>Last Name</label>
-                       <input type = "text" name= "lastname" onChange={this.handleChange} value={this.state.lastname} ></input>
-                       <label>Username</label>
-                       <input type = "text" name= "username" onChange={this.handleChange} value={this.state.username} ></input>
-                       <label>Password</label>
-                       <input type = "text" name= "password" onChange={this.handleChange} value={this.state.password} ></input>
-                       <label>Email</label>
-                       <input type = "text" name= "email" onChange={this.handleChange} value={this.state.email} ></input>
-                       <label>Phone Number</label>
-                       <input type = "text" name= "phonenumber" onChange={this.handleChange} value={this.state.phonenumber} ></input>
-                       <button type="submit" className="btn btn-primary btn-block">Register</button>
-                   </table>
-               </form>
+<form onSubmit={this.handleSubmit}>
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+          <div class="card-body p-5 text-center">
 
-           </div>
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Please enter registration information!</p>
+            
+
+            <div class="form-outline form-white mb-4">
+                <input type="firstName" id="typeFirstNameX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.firstname} />
+                <label class="form-label" for="typeFirstNameX">First Name</label>
+            </div>
+
+            <div class="form-outline form-white mb-4">
+                <input type="lastName" id="typeLastNameX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.lastname} />
+                <label class="form-label" for="typeLastNameX">Last Name</label>
+            </div>
+
+            <div class="form-outline form-white mb-4">
+                <input type="phoneNumber" id="typePhoneNumberX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.phonenumber} />
+                <label class="form-label" for="typePhoneNumberX">Phone Number</label>
+            </div>
+
+            <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.email} />
+                <label class="form-label" for="typeEmailX">Email</label>
+            </div>
+
+            <div class="form-outline form-white mb-4">
+                <input type="username" id="typeUserNameX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.username} />
+                <label class="form-label" for="typeUserNameX">User Name</label>
+            </div>
+
+            <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.password} />
+                <label class="form-label" for="typePasswordX">Password</label>
+            </div>
+  
+            <button class="btn btn-outline-light btn-lg px-5" type="submit">Register</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</form>
+
         )
     }
 }
