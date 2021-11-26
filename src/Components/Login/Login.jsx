a93286d7e87cda0df8424009fc3a774519e296de
Marshall C <MCass2101@gmail.com>
2021-11-26 11:05:19 -0500

    new login form created

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'react-router-dom';
 import './Login.css';
 
 
@@ -34,46 +35,42 @@ class Login extends Component {
 
     render() {
         return (
-<form onSubmit={this.handleSubmit}>
-<section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style={{borderRadius: "1rem"}}>
-          <div class="card-body p-5 text-center">
+          
+<div className="container">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+<form onSubmit={this.handleSubmit} className="form-signin">
+          <div className="row">
+            <div className="col">
+                <div>
+                <h1 className="fs-1 fw-bold mb-5 text-uppercase">Login</h1>
+                </div>
+                  <p className="fs-5 text-white-50 mb-4">Please enter your username and password!</p>
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your username and password!</p>
+                  <div className="form-outline form-white mb-4">
+                    <input type="text" name= "username" id="typeUsernameX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.username}  />
+                    <label className="form-label fs-5" for="typeUsernameX">Username</label>
+                  </div>
 
-              <div class="form-outline form-white mb-4">
-                <input type="text" name= "username" id="typeUsernameX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.username}  />
-                <label class="form-label" for="typeUsernameX">Username</label>
-              </div>
-
-              <div class="form-outline form-white mb-4">
-                <input type="password" name= "password" id="typePasswordX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.password} />
-                <label class="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
-              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
+                  <div className="form-outline form-white mb-4">
+                    <input type="password" name= "password" id="typePasswordX" class="form-control form-control-lg" onChange={this.handleChange} value={this.state.password} />
+                    <label className="form-label fs-5" for="typePasswordX">Password</label>
+                  </div>
+                  <div>
+                  <button class="btn btn-outline-light btn-lg px-5 fw-" type="submit">Login</button>
+                  <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+                  </div>
+                  <div>
+                  <p class="fs-6 fw-bold mb-0">Don't have an account? <a href="#!" className="fs-6 text-white-50 fw-bold">Sign Up</a></p>
+                  </div>
             </div>
-
-            <div>
-              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a></p>
-            </div>
-
+            
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+          
 </form>
+
+</div>
+
+
         )
     }
 }
